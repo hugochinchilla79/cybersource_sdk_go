98b65db1c34ff1650bb405525cdf688eaf281350
Hugo <shugodestro@gmail.com>
2026-01-30 14:23:57 -0600

    Updating processing information to capture false by default

--- models/processing_information.go
+++ models/processing_information.go
@@ -2,6 +2,6 @@ package models
 
 type ProcessingInformation struct {
 	ActionList        []string `json:"actionList,omitempty"`
-	Capture           bool     `json:"capture,omitempty"`
+	Capture           *bool    `json:"capture,omitempty"`
 	CommerceIndicator string   `json:"commerceIndicator,omitempty"`
 }
