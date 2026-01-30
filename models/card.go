e3416e69e51b402721cd075235623cbd8e0ccbcd
Hugo <shugodestro@gmail.com>
2026-01-30 14:36:12 -0600

    Adding SecurityCodeIndicator property

--- models/card.go
+++ models/card.go
@@ -11,5 +11,6 @@ type Card struct {
 	SecurityCode    string `json:"securityCode,omitempty"`
 	// Type is the card type code. Required for Cartes Bancaires and UPI.
 	// Common values: 001=Visa, 002=Mastercard, 003=Amex, 004=Discover
-	Type string `json:"type,omitempty"`
+	Type                  string `json:"type,omitempty"`
+	SecurityCodeIndicator string `json:"securityCodeIndicator,omitempty"`
 }
